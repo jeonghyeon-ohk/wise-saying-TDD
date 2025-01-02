fd31e9036d246b1bc27badf1b33ce0a2725d2187
jeonghyeon-ohk <ok6737@naver.com>
2025-01-02 18:55:21 +0900

    System.setOut으로 출력값 가져오기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,38 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner("aaa\nbbb\n");
+        test2();
+    }
+    public static void test2(){
+        PrintStream origin = System.out;
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        System.out.println("hello"); // 출력 안됨. 왜?
+        // -> hello 는 out에 들어있기 때문
+
+        System.setOut(origin);
+
+        String str = out.toString();
+        System.out.println(str);
+    }
+
+    public static void test1(){
+        Scanner scan = new Scanner("등록\n현재를 사랑하라.\n작자미상\n");
         String val1 = scan.nextLine();
         System.out.println(val1);
+
         String val2 = scan.nextLine();
         System.out.println(val2);
+
+        String val3 = scan.nextLine();
+        System.out.println(val3);
+
+        TestApp testApp = new TestApp();
+        testApp.run();
     }
 }
--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,13 +2,39 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FirstTest {
 
     @Test
-    @DisplayName("first test")
     void t1(){
-        assertThat(1).isEqualTo(1);
+        int rst = 1;
+        assertThat(rst).isEqualTo(1);
+    }
+
+    @Test
+    void t2(){
+        TestApp app = new TestApp();
+        app.run();
+    }
+
+    @Test
+    void t3(){
+        // 테스트봇 선입력
+        Scanner sc = new Scanner("종료\n");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        TestApp app = new TestApp();
+        app.run();
+
+        assertThat(out.toString()).contains("명언앱을 종료합니다.");
+
+        // 출력값을 체크
     }
 }
