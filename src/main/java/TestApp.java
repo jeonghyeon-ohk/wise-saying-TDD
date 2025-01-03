f9a7b616968d62e3f9aeaa6cbde6864e092d2004
jeonghyeon-ohk <ok6737@naver.com>
2025-01-03 12:46:19 +0900

    t5  green

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -2,6 +2,10 @@ public class TestApp {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        System.out.println("명언 : ");
+        System.out.println("작가 : ");
+
         System.out.println("명언앱을 종료합니다.");
 
     }
