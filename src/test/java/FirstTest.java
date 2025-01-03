82faf929fc5745783fc18fa188f203faad187230
jeonghyeon-ohk <ok6737@naver.com>
2025-01-03 12:43:35 +0900

    t5 red

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.Scanner;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FirstTest {
@@ -27,16 +23,9 @@ public class FirstTest {
 
     @Test
     void t3() {
-        // 테스트봇 선입력
-        Scanner sc = new Scanner("종료\n");
+        String out = TestBot.run("종료");
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        TestApp app = new TestApp();
-        app.run();
-
-        assertThat(out.toString()).contains("명언앱을 종료합니다.");
+        assertThat(out).contains("명언앱을 종료합니다.");
 
         // 출력값을 체크
     }
@@ -45,20 +34,21 @@ public class FirstTest {
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
     void t4() {
         // 테스트봇 선입력
-        Scanner sc = new Scanner("종료\n");
+        String out = TestBot.run("종료");
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        TestApp app = new TestApp();
-        app.run();
-
-
-        // 테스트 코드 작성이 어려움.
-        // assertJ 를 사용법을 잘 모름. -> GTP를 활용하자
-
-        assertThat(out.toString())
+        assertThat(out)
                 .containsSubsequence("== 명언 앱 ==", "명언앱을 종료합니다.");
+
+    }
+
+    @Test
+    @DisplayName("등록 - 명언 1개 입력")
+    void t5() {
+        // 테스트봇 선입력
+        String out = TestBot.run("등록\n현재를 사랑하라.\n작자미상\n종료");
+
+        assertThat(out)
+                .containsSubsequence("명언 : ", "작가 : ");
 
     }
 }
